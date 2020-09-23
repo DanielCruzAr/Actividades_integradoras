07b2acf97688233f7f89ec6de717fe8cfa1e40bf
DanielCruzAr <58478374+DanielCruzAr@users.noreply.github.com>
2020-09-22 21:06:04 -0500

    Add files via upload

--- Act_integral1/logs.h
+++ Act_integral1/logs.h
@@ -13,22 +13,29 @@ class Logs{
     private:
 		string file;
 		vector<string> data;
+		int error; //esta variable fue creada porque hay un error en el archivo en el primer renglon
 		
 		//funciones
+		//Ordenamiento
 		void swap(int,int);
+		void swap_int(vector<int>&,int,int);
 		void new_vec(vector<int>&);
+		//Busqueda
+		void track_error(); //funcion para rastrear el error
+		void str_vec(vector<string>&);
 	public:
 		Logs(string);
 		
 		void lee();
 		void imprime();
-		//void ordena();
-		void busca(string);
+		void ordena();
+		string busca(string);
 		void escribe();
 };
 
 Logs::Logs(string f){
 	file = f;
+	error = 0;
 }
 
 //Lectura de archivo
@@ -52,7 +59,6 @@ void Logs::lee(){
 //Impresion en consola
 
 void Logs::imprime(){
-    cout << data[0] << endl;
 	cout << "Hora | Nombre | Direccion | Motivo visita \n";
 	for (string str : data){
 		cout << str << "\n";
@@ -68,16 +74,32 @@ void Logs::swap(int i, int j){
 	data[j] = aux;
 }
 
+void Logs::swap_int(vector<int> &v, int i, int j){
+	int aux = v[i];
+	v[i] = v[j];
+	v[j] = aux;
+}
+
 void Logs::new_vec(vector<int> &v){
 	string line;
 	int num;
 	
-	for (int i = 1; i<data.size() ; i++){
+	
+	for (int i = 0; i<data.size() - 1; i++){
 		line = "";
-		for(int j = 0; j <= 2; j++){
-			line = line + data[i][j];
+		if(i == error){
+			line += data[i][2];
+			line += data[i][4];
+			line += data[i][8];
+			line += data[i][10];
 		}
-		cout << line << endl;
+		else{
+			line += data[i][1];
+			line += data[i][3];
+			line += data[i][7];
+			line += data[i][9];
+		}
+	     
 		num = stoi(line);
 		v.push_back(num);
 	}
@@ -91,14 +113,61 @@ void Logs::ordena(){
 		for (int j = 0; j < i; j++) {
 			if (int_vec[j] > int_vec[j + 1]) {
 				swap(j, j + 1);
+				swap_int(int_vec, j, j+1);
 			}
 		}
 	}
+	
 }
 
 //Busqueda
 
-void Logs::busca(string n){}
+void Logs::track_error(){
+	string track;
+	for(int i=0; i < data.size() - 1; i++){
+		track = data[i][2];
+		if (track == "1"){
+			error = i;
+		}
+	}
+}
+
+void Logs::str_vec(vector<string> &strv){
+	string hr;
+	
+	for (int i = 0; i<data.size() - 1; i++){
+		hr = "";
+		if(i == error){
+			hr += data[i][2];
+			hr += data[i][4];
+			hr += data[i][6];
+			hr += data[i][8];
+			hr += data[i][10];
+		}
+		else{
+			hr += data[i][1];
+			hr += data[i][3];
+			hr += data[i][5];
+			hr += data[i][7];
+			hr += data[i][9];
+		}
+		
+		strv.push_back(hr);
+	}
+}
+
+string Logs::busca(string n){
+	vector<string> strv;
+	track_error();
+	str_vec(strv);
+	
+	for (int i = 0; i < strv.size() - 1; i++){
+		if (strv[i] == n){
+			return data[i];
+		}
+	}
+	return "No hay registros a esa hora \n";
+}
 
 //Escritura de archivo
 
--- Act_integral1/main.cpp
+++ Act_integral1/main.cpp
@@ -4,13 +4,44 @@
 #include "logs.h"
 using namespace std;
 
+void menu(){
+	cout << "Opcion 1: ordenar por hora" << endl;
+	cout << "Opcion 2: buscar por hora" << endl;
+	cout << "Opcion 3: imprimir archivo en consola" << endl;
+	cout << "Opcion 4: escribir en nuevo archivo de texto" << endl;
+	cout << "Opcion 5: salir" << endl;
+}
+
 int main(){
+	int opcion = 0;
+	string n;
 	Logs log("datos.txt");
 	
 	log.lee();
-	//log.ordena();
-	//log.busca(string n);
-	log.imprime();
-	log.escribe();
+	
+	while(opcion <5 && opcion >-1){
+      menu();
+      cin >> opcion;
+
+      switch(opcion){
+		  case 1:
+			 log.ordena();
+			 break;
+			 
+		  case 2:
+			 cout << "introduce hora de registro (hh:mm): \n";
+			 cin >> n;
+			 cout << log.busca(n) << endl; 
+			 break;
+			 
+		  case 3:
+			 log.imprime();
+			 break;
+		  
+		  case 4:
+			 log.escribe();
+			 break;
+		}
+	}
 	return 0;
 }
